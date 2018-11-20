49e72f798d508511d697ea41ccfc25ccb500d193
Ajeet Yadav <38289540+ajitjss@users.noreply.github.com>
2018-11-20 23:52:36 +0530

    new code

---
+++ src/date/testDate.java
@@ -0,0 +1,12 @@
+package date;
+
+import java.util.Date;
+
+public class testDate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Date date = new Date();
+System.out.println(date.toString());
+	}
+}
---
+++ src/stringManoulation/test1.java
@@ -0,0 +1,25 @@
+package stringManoulation;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+String str1= "Ajit";
+String str2 = "Analytical";
+System.out.println("index"+str1.indexOf('i'));
+System.out.println(str1.indexOf("Edu"));
+int count;
+System.out.println(str1.valueOf('a'));
+//System.out.println(str1.indexOf('A', 3));
+/*int a=1;
+int b =5;
+if(str1.valueOf(a>b)) {
+System.out.println("True");
+	}
+else
+	System.out.println("fail");
+	*/
+}
+}
