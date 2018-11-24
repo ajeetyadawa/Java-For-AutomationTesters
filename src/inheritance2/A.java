ba00ce059f68eab1fafcc4de8353b926c8b3ea46
Ajeet Yadav <38289540+ajitjss@users.noreply.github.com>
2018-11-24 23:15:33 +0530

    Inheritance

---
+++ src/inheritance2/A.java
@@ -0,0 +1,10 @@
+package inheritance2;
+
+public class A {
+
+	int i, j;
+	void showij() {
+		System.out.println("i and j :"+i +" "+j);
+			}
+	}
+
---
+++ src/inheritance2/B.java
@@ -0,0 +1,11 @@
+package inheritance2;
+
+public class B extends A {
+int k;
+void showk() {
+	System.out.println("i+j+k: "+(i+j+k));
+	}
+	void sum() {
+		System.out.println("i+j+k: "+(i+j+k));
+			}
+}
