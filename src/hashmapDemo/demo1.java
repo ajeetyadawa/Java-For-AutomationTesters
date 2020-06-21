d78082b1c62e14a16789cdade0d23fab77aff72a
Ajeet Yadav <38289540+ajitjss@users.noreply.github.com>
2020-06-21 12:37:13 +0530

    updated

--- src/hashmapDemo/demo1.java
+++ src/hashmapDemo/demo1.java
@@ -7,9 +7,9 @@ public class demo1 {
 
 	public static void main(String[] args) {
 		Map<String, String> userDetails = new HashMap();
-		userDetails.put("userName", "Soumya");
+		userDetails.put("userName", "Animesh");
 		userDetails.put("Age", "26");
-		userDetails.put("City", "Pune");
+		userDetails.put("City", "Ballia");
 		userDetails.put("MobileNumber", "815000");
 		String userName= userDetails.get("userName");
 		System.out.println("userName "+ userName);
@@ -22,12 +22,16 @@ public class demo1 {
 		userDetails2.put("userName", "Ajeet");
 		userDetails2.put("Age", "26");
 		userDetails2.put("City", "Delhi");
-		userDetails2.put("MobileNumber", "95555");
-		
+		userDetails2.put("MobileNumber", "9555902032");
+		System.out.print("Is hashMap have Key"+userDetails2.containsKey("Age"));
+		System.out.println("is HashMap Empty"+userDetails2.isEmpty());
 		System.out.println("User 2 user Age : "+userDetails2.get("Age"));
 		System.out.println("User 2 user City : "+userDetails2.get("City"));
-		
-		
+	//userDetails2.remove("userName");
+		//System.out.println("User 2 details without "+userDetails2.get("userName"));
+	//	userDetails2.clear();
+		userDetails2.replace("userName", "Amit");
+		System.out.println("User 2 details"+userDetails2);
 	}
 
 }
